16f63e33a5dd7a828358b25b5152056e4678011f
JesusT <core.jesust@gmail.com>
2020-09-07 12:54:01 +0200

    Agregada lógica, y algunos ajustes a la vista

--- app/src/main/java/com/example/lunes2w_diseovista/MainActivity.java
+++ app/src/main/java/com/example/lunes2w_diseovista/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.lunes2w_diseovista;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        counter = 0;
+        showCounter();
+    }
+
+    public void increment(View view) {
+        counter++;
+        showCounter();
+    }
+
+    public void decrement(View view) {
+        counter--;
+        showCounter();
+    }
+
+    public void reset(View vista) {
+        counter = 0;
+        showCounter();
+    }
+
+    public void showCounter() {
+        TextView status;
+        status = (TextView) findViewById(R.id.result);
+        status.setText(String.valueOf(counter));
     }
 }
+
